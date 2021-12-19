13b4d67ba94bee0b6e39f893042ec59f085bb18f
David Arias <61300552+devarias@users.noreply.github.com>
2021-12-19 17:24:50 -0500

    Solution challenge day 11

---
+++ 2021/day11.js
@@ -0,0 +1,9 @@
+export default function shouldBuyFidelity(times) {
+  console.log('t', times);
+  const withOutCard = 12 * times;
+  const withCard = Array.from({length: times}, (_, i) => i + 1).reduce((acc, t) => {
+    acc += 12 * Math.pow(0.75, t);
+    return acc;
+  }, 250);
+  return withCard < withOutCard;
+}
