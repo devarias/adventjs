d990e52a31eacb065a189384994b30697508c4eb
David Arias <61300552+devarias@users.noreply.github.com>
2021-12-19 17:25:23 -0500

    Remove console.log

--- 2021/day11.js
+++ 2021/day11.js
@@ -1,5 +1,4 @@
 export default function shouldBuyFidelity(times) {
-  console.log('t', times);
   const withOutCard = 12 * times;
   const withCard = Array.from({length: times}, (_, i) => i + 1).reduce((acc, t) => {
     acc += 12 * Math.pow(0.75, t);
